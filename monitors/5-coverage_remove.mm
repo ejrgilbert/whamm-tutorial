cd6d1af2ef18f034b7b876e2ee0c5b21266f5344
Elizabeth Gilbert <evgilber@andrew.cmu.edu>
2025-10-13 00:20:42 -0400

    fix the run commands

--- monitors/5-coverage_remove.mm
+++ monitors/5-coverage_remove.mm
@@ -6,9 +6,7 @@
 // See: https://github.com/titzer/wizard-engine/blob/a3bc0669550a874d29742a168a5bc8e5b461d6ab/apis/whamm_dyninstr.v3#L6
 // To use this as a whamm library, we supply the API definition in the libs/dyninstr.wasm module.
 
-// TO RUN (rewriting):
-// ./bin/whamm/whamm-mac instr --script monitors/5-coverage_remove.mm --app apps/branches.wasm --user-libs dyninstr=./libs/dyninstr.wasm
-// bin/wizeng/wizeng.jvm --ext:multi-memory --env=TO_CONSOLE=true libs/whamm_core.wasm libs/dyninstr.wasm output.wasm
+// TO RUN (dynamic probe removal is not possible with rewriting)
 
 // TO RUN (engine):
 // ./bin/whamm/whamm-mac instr --script monitors/5-coverage_remove.mm --user-libs dyninstr=./libs/dyninstr.wasm --wizard
